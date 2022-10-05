9e7293e19818afa2a7b0dca8b1b761777be11a69
boomlee <boomlee2410@gmail.com>
2022-10-05 13:14:40 +0700

    configuring react

--- src/App.js
+++ src/App.js
@@ -1,5 +1,33 @@
 import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import { Navbar, NavbarBrand } from 'reactstrap';
+
+class App extends Component {
+
+  render() {
+
+    return (
+
+      <div className="App">
+
+        <Navbar dark color="primary">
+
+          <div className="container">
+
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+
+          </div>
+
+        </Navbar>
+
+      </div>
+
+    );
+
+  }
+
+}
 
 function App() {
   return (
@@ -21,5 +49,6 @@ function App() {
     </div>
   );
 }
+
 
 export default App;
